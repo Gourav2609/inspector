f17c02aa5d87d0211750cb576e49009d52b2409b
Gourav2609 <herculeswarrior.in@gmail.com>
2024-01-06 14:41:01 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Package from "./assets/new.jpg"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={Package}></img>
+      </div>
+      <div>
+      <h1>Here is the package</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
